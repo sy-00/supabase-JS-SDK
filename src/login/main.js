2159b55c958270ae73c9a961dc552cbb5904ef7d
sy-00 <sylwia3p@wp.pl>
2025-06-03 18:39:26 +0200

    feat: created login page

--- src/login/main.js
+++ src/login/main.js
@@ -1,1 +1,25 @@
-console.log('nested');
+import { supabase } from '../api-client.js';
+
+const form = document.getElementById('login-form');
+const message = document.getElementById('login-message');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const email = form.email.value;
+  const password = form.password.value;
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    message.textContent = 'Błędne dane logowania.';
+  } else {
+    message.textContent = 'Zalogowano.'
+    setTimeout(() => {
+      window.location.href = '../index.html';
+    }, 1000);
+  }
+});
